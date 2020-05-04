44a4fd6e191859b33eb4658da33246850e2106fc
MarceloGomez92 <64440327+MarceloGomez92@users.noreply.github.com>
2020-05-04 12:42:54 -0500

    Add files via upload

---
+++ Concatenar.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 12:33:10 2020
+
+@author: Marcelo Gómez
+"""
+
+str1="Cisco"
+str2="Networking"
+str3="Academy"
+space=" "
+print(str1+space+str2+space+str3)
+print("\n"*2)
+print(str1+str2+str3)
+print("\n"*2)
+print(str1,str2,str3)
