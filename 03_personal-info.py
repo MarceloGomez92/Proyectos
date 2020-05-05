574dbadeb3b61886ecc7259e4aa7c899740a993f
MarceloGomez92 <64440327+MarceloGomez92@users.noreply.github.com>
2020-05-04 20:31:03 -0500

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 15:02:12 2020
+
+@author: dell
+"""
+
+
+nombre=input("Cual es tu nombre? ")
+print("\n")
+apellido=input("Cual es tu apellido? ")
+print("\n")
+lugar=input("Donde vives? ")
+print("\n")
+edad=input("Cual es tu edad? ")
+space=" "
+print("\n")
+print("Buenas tardes"+space+nombre+space+apellido+space+"vives en"+space+lugar+space+"y tienes"+space+edad+space+"años")
